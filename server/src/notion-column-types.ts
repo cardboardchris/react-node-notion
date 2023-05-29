6655145bef7d1d384c90087babde761bd7a339f5
Chris Metivier <chris.metivier@gmail.com>
2023-05-28 22:50:34 -0400

    changed some property names

--- server/src/notion-column-types.ts
+++ server/src/notion-column-types.ts
@@ -1,17 +1,20 @@
-export const columnTypes = {
-  "name": {
-    "dataType": "string",
-    "notionPropertyType": "title",
-    "propertyPath": "title[0].plain_text"
+export const notionColumnTypes: any[] = [
+  {
+    columnType: "name",
+    dataType: "string",
+    propertyType: "title",
+    propertyPath: "title[0].plain_text"
   },
-  "text": {
-    "dataType": "string",
-    "notionPropertyType": "rich_text",
-    "propertyPath": "rich_text[0].plain_text"
+  {
+    columnType: "text",
+    dataType: "string",
+    propertyType: "rich_text",
+    propertyPath: "rich_text[0].plain_text"
   },
-  "formula": {
-    "dataType": "string",
-    "notionPropertyType": "formula",
-    "propertyPath": "formula.string"
-  }
-}
+  {
+    columnType: "formula",
+    dataType: "string",
+    propertyType: "formula",
+    propertyPath: "formula.string"
+  },
+]
