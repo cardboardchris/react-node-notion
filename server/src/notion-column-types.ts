bf9f06320427247bf9aac77593cfc4e72a16c0ab
Chris Metivier <chris.metivier@gmail.com>
2023-05-30 22:15:00 -0400

    renamed properties and tried without array accessors

--- server/src/notion-column-types.ts
+++ server/src/notion-column-types.ts
@@ -1,20 +1,31 @@
-export const notionColumnTypes: any[] = [
+// Define the type of our notion column attributes object
+export type NotionDBProperty = {
+  columnType: string;
+  dataType: string;
+  propertyType: string;
+  propertyValuePath: string;
+};
+
+// Define an array of known column attributes from notion
+export const notionDBPropertyAttributes: NotionDBProperty[] = [
   {
     columnType: "name",
     dataType: "string",
     propertyType: "title",
-    propertyPath: "title[0].plain_text"
+    propertyValuePath: "title.plain_text"
+    // propertyValuePath: "title[0].plain_text"
   },
   {
     columnType: "text",
     dataType: "string",
     propertyType: "rich_text",
-    propertyPath: "rich_text[0].plain_text"
+    propertyValuePath: "rich_text.plain_text"
+    // propertyValuePath: "rich_text[0].plain_text"
   },
   {
     columnType: "formula",
     dataType: "string",
     propertyType: "formula",
-    propertyPath: "formula.string"
+    propertyValuePath: "formula.string"
   },
 ]
