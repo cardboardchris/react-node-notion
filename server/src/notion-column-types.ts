b93a3c065da32b565102ac8efe288e47e81cfaf1
Chris Metivier <chris.metivier@gmail.com>
2023-06-03 18:41:21 -0400

    successfully returns values to console

--- server/src/notion-column-types.ts
+++ server/src/notion-column-types.ts
@@ -3,7 +3,7 @@ export type NotionDBProperty = {
   columnType: string;
   dataType: string;
   propertyType: string;
-  propertyValuePath: string;
+  propertyValueType: string;
 };
 
 // Define an array of known column attributes from notion
@@ -12,20 +12,18 @@ export const notionDBPropertyAttributes: NotionDBProperty[] = [
     columnType: "name",
     dataType: "string",
     propertyType: "title",
-    propertyValuePath: "title.plain_text"
-    // propertyValuePath: "title[0].plain_text"
+    propertyValueType: "plain_text"
   },
   {
     columnType: "text",
     dataType: "string",
     propertyType: "rich_text",
-    propertyValuePath: "rich_text.plain_text"
-    // propertyValuePath: "rich_text[0].plain_text"
+    propertyValueType: "plain_text"
   },
   {
     columnType: "formula",
     dataType: "string",
     propertyType: "formula",
-    propertyValuePath: "formula.string"
+    propertyValueType: "string"
   },
 ]
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,24 +1,26 @@
-import { config } from './notion-config';
-import { NotionDBProperty, notionDBPropertyAttributes } from './notion-column-types';
-import http from 'http';
-import { Client } from '@notionhq/client';
+import { config } from "./notion-config";
+import {
+  NotionDBProperty,
+  notionDBPropertyAttributes,
+} from "./notion-column-types";
+import http from "http";
+import { Client } from "@notionhq/client";
 
 function getValueFromNotionDBRow(object: any[], propertyString: string) {
-  const properties: string[] = propertyString.split('.');
+  const properties: string[] = propertyString.split(".");
   let value: any = object;
 
   // for (let i = 0; i < properties.length; i++) {
-    // TODO get the rest of the way down the accessor chain
-    // accounting for if the value is null at an earlier property in the chain
-  for (let i = 0; i < 3; i++) {
+  // TODO get the rest of the way down the accessor chain
+  // accounting for if the value is null at an earlier property in the chain
+  for (let i = 0; i < 2; i++) {
     const property = properties[i];
     value = value[property];
 
     if (value === undefined) {
       // Handle invalid property path
       return undefined;
-    }
-    else if (Array.isArray(value)) {
+    } else if (Array.isArray(value)) {
       value = value[0];
     }
   }
@@ -27,22 +29,25 @@ function getValueFromNotionDBRow(object: any[], propertyString: string) {
 }
 
 /**
- * @param columnType 
+ * @param columnType
  * @returns notionDBProperty
  */
 function getNotionDBPropertyAttributesByColumnType(columnType: string) {
-  return notionDBPropertyAttributes.find(notionDBProperty => notionDBProperty.columnType === columnType);
-};
+  return notionDBPropertyAttributes.find(
+    (notionDBProperty) => notionDBProperty.columnType === columnType
+  );
+}
 
 interface NotionColumn {
-  notionColumnName: string,
-  notionColumnType: string,
+  notionColumnName: string;
+  notionColumnType: string;
 }
 
 const notionColumnsToGet: NotionColumn[] = [
-  { notionColumnName: 'Name', notionColumnType: 'name' },
-  { notionColumnName: 'Ability text', notionColumnType: 'formula' },
-  { notionColumnName: 'Instructions', notionColumnType: 'text' }
+  { notionColumnName: "Name", notionColumnType: "name" },
+  { notionColumnName: "Ability text", notionColumnType: "formula" },
+  { notionColumnName: "Instructions", notionColumnType: "text" },
+  { notionColumnName: "Image", notionColumnType: "files" },
 ];
 
 // get our database configuration from notion-config.js
@@ -54,7 +59,7 @@ const notionSecret = config.notionSecretKey;
 // with their Notion data in it
 if (!notionDBId || !notionSecret) {
   throw Error(
-    'No keys defined. Define notionSecretKey and database IDs in notion-config.ts'
+    "No keys defined. Define notionSecretKey and database IDs in notion-config.ts"
   );
 }
 
@@ -63,108 +68,77 @@ const notion = new Client({
   auth: notionSecret,
 });
 
-const host = 'localhost';
+const host = "localhost";
 const port = 8000;
 
 // Require an async function here to support await with the DB query
 const server = http.createServer(async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
 
   switch (req.url) {
-    case '/':
+    case "/":
       // Query the database and wait for the result
       const query = await notion.databases.query({
         database_id: notionDBId,
+        sorts: [
+          {
+            property: "Name",
+            direction: "ascending",
+          },
+        ],
       });
-
-      const results: any[] = query.results;
-
-      const getNotionDBPropertyAttributesByColumnType = (columnType: string) => {
-        return notionDBPropertyAttributes.find(notionDBProperty => notionDBProperty.columnType === columnType);
-      };
 
       // We map over the complex shape of the results and return an array of
       // values from each column of the desired columns (notionColumnsToGet)
-      const list: any[][] = query.results.map((row: any) => {
-
+      const list: string[][] = query.results.map((row: any) => {
         // get the plain text data from the matching column in the current row
-        const allColumnsPropertyValues: string[] = notionColumnsToGet.map((column: NotionColumn) => {
-          // get the notionColumnType of the column, then its property path
-          const notionProperty: NotionDBProperty|undefined = getNotionDBPropertyAttributesByColumnType(column.notionColumnType);
-          // const notionPropertyValuePath: string|undefined = getNotionDBPropertyAttributesByColumnType(column.notionColumnType)?.propertyValuePath;
-          if (notionProperty) {
-            const notionPropertyValuePath: string = notionProperty.propertyValuePath;
-            const columnName: string = column.notionColumnName;
-            const accessorString = `properties.${columnName}.${notionPropertyValuePath}`;
+        const allColumnsPropertyValues: string[] = notionColumnsToGet.map(
+          (column: NotionColumn) => {
+            // get the notionColumnType of the column, then its property path
+            const notionProperty: NotionDBProperty | undefined =
+              getNotionDBPropertyAttributesByColumnType(
+                column.notionColumnType
+              );
+            if (notionProperty) {
+              const notionPropertyType: string = notionProperty.propertyType;
+              const notionPropertyValueType: string =
+                notionProperty.propertyValueType;
+              const columnName: string = column.notionColumnName;
+              const propertyValueParent =
+                row.properties[columnName][notionPropertyType];
 
-            // return accessorString;
-            return getValueFromNotionDBRow(row, accessorString);
-            // return row.properties[column.notionColumnName];
-            // return row.properties[column.notionColumnName][notionPropertyValuePath];
+              let propertyValueObject: { [key: string]: string } = {};
+
+              if (Array.isArray(propertyValueParent)) {
+                propertyValueObject = propertyValueParent[0];
+              } else {
+                propertyValueObject = propertyValueParent;
+              }
+
+              if (propertyValueObject) {
+                return propertyValueObject[notionPropertyValueType];
+              }
+
+              return `value for "${column.notionColumnName}" not found on this row`;
+            }
+
+            // if the notion database property doesn't exist in notionDBPropertyAttributes, return an error message
+            return `properties definition for Notion property type '${column.notionColumnType}' not found in notion db properties array (notionDBPropertyAttributes)`;
           }
-
-          // if the notion database property doesn't exist in notionDBPropertyAttributes, return an error message
-          return 'could not get property value path';
-        });
-
-        // const notionValuePath: string = columnAttributes.propertyPath;
+        );
 
         return allColumnsPropertyValues;
-        // // for each row, get the cells where the columns we specified
-        // // in notionColumnsToGet above intersect with that row.
-
-        // // to do that, map over notionColumnsToGet and generate an object with
-        // // a cell name (the same name as the column)
-        // // and a cell value (the property of the row that matches the column name)
-        // const cells: DatabaseCell[] = notionColumnsToGet.map((column: NotionColumnType) => {
-        //   const columnName: string = column.notionColumnName;
-        //   const cellValue: any[] = row.properties[column.notionColumnName];
-        //   // cellValue will be an object with shape { id: string, type: string, ... }
-        //   return { columnName, cellValue };
-        // });
-
-        // // since different column 'types' (dataTypes) in notion have different data types,
-        // // we should verify that each cell value's data 'type' matches the type given
-        // // in the notionColumnAttributes array for that type of column
-
-        // let passesTypeTest: boolean = true;
-
-        // cells.every((cell: DatabaseCell) => {
-        //   // for each cell, get the column name of our desired data
-        //   const notionColumn = notionColumnsToGet.find((column) => {
-        //     column.notionColumnName === cell.columnName
-        //   });
-        //   if (notionColumn) {
-        //     const notionColumnType = notionColumnAttributes.find((type) => {
-        //       type.columnType === notionColumn;
-        //     });
-        //     passesTypeTest = cell.cellValue.type === notionColumnAttributes[notionColumnType].propertyType;
-        //   }
-        //   return passesTypeTest;
-        // })
-
-        // if (passesTypeTest) {
-        //   return cells;
-        // }
-
-        // // If a row is found that does not match the rules we checked it will still return in the
-        // // the expected shape but with a NOT_FOUND label
-        // return {
-        //   name: 'NOT_FOUND',
-        //   ability: 'NOT_FOUND',
-        //   instructions: 'NOT_FOUND',
-        // };
       });
 
-      res.setHeader('Content-Type', 'application/json');
+      res.setHeader("Content-Type", "application/json");
       res.writeHead(200);
       res.end(JSON.stringify(list));
       break;
 
     default:
-      res.setHeader('Content-Type', 'application/json');
+      res.setHeader("Content-Type", "application/json");
       res.writeHead(404);
-      res.end(JSON.stringify({ error: 'Resource not found' }));
+      res.end(JSON.stringify({ error: "Resource not found" }));
   }
 });
 
